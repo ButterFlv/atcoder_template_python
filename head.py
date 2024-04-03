428a452599b09cd948925b2b1755055ebf2109f4
ButterFlv <122111975+ButterFlv@users.noreply.github.com>
2024-04-03 19:34:51 +0900

    Create head.py

---
+++ head.py
@@ -0,0 +1,13 @@
+S = input
+R = range
+P = print
+def I(): return int(S())
+def M(): return map(int, S().split())
+def L(): return list(M())
+def O(): return list(map(int, open(0).read().split()))
+def yn(b): print("Yes" if b else "No")
+biga = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+smaa = "abcdefghijklmnopqrstuvwxyz"
+
+
+
