f5d0e3944a30ffcea7a7f4724d1a0710701c08c9
ButterFlv <122111975+ButterFlv@users.noreply.github.com>
2024-06-04 22:01:06 +0900

    Update doubling.py

--- doubling.py
+++ doubling.py
@@ -1,6 +1,7 @@
 # _size: 操作を行う要素の集合のサイズ (各値は i (0 <= i < _size))
 # _f: 集合から集合の写像 (i (0 <= i < _size) に対して定義される)
 # _repeat: 操作を行う回数
+# dp[i][j]: 要素 j から 2^i 回遷移した要素
 def doubling(_size, _f, _repeat):
   _times = 1
   while 2 ** (_times - 1) <= _repeat: _times += 1
