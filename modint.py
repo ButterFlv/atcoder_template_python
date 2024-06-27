f507143d30902ff1cfc40f97a80e936ac58fdce1
ButterFlv <122111975+ButterFlv@users.noreply.github.com>
2024-06-27 14:58:42 +0900

    Update modint.py

--- modint.py
+++ modint.py
@@ -1,53 +1,34 @@
 class mint:
-  inv = None
-  MOD = None
-
-  def init(MOD = 998244353, inv_generate = 1):
-    mint.MOD = MOD
-    mint.inv = [None]*(inv_generate + 1)
-    mint.inv[1] = 1
-    for a in range(2, inv_generate + 1):
-      mint.inv[a] = ( -(MOD//a) * mint.inv[MOD%a] ) % MOD
+  MOD = 998244353
 
   def __init__(self, n): self.n = n % self.MOD
 
   def __str__(self): return str(self.n)
   __repr__ = __str__
 
-  def __add__(self, other):
-    return mint(self.n + other.n) if isinstance(other, mint) else mint(self.n + other)
-  def __sub__(self, other):
-    return mint(self.n - other.n) if isinstance(other, mint) else mint(self.n - other)
-  def __mul__(self, other):
-    return mint(self.n * other.n) if isinstance(other, mint) else mint(self.n * other)
-  def __truediv__(self, other):
-    a, b = self.n, other.n if isinstance(other, mint) else other % self.MOD
-    return mint(a * self.inv[b]) if b<len(self.inv) else mint(a * self.inv_extgcd(b))
-  def __pow__(self, other):
-    return mint(pow(self.n, other.n, self.MOD)) if isinstance(other, mint) else mint(pow(self.n, other, self.MOD))
-
-  __radd__ = __add__
-  def __rsub__(self, other):
-    return mint(other.n - self.n) if isinstance(other, mint) else mint(other - self.n)
-  __rmul__ = __mul__
-  def __rtruediv__(self, other):
-    a, b = other.n if isinstance(other, mint) else other, self.n
-    return mint(a * self.inv[b]) if b<len(self.inv) else mint(a * self.inv_extgcd(b))
-  def __rpow__(self, other):
-    return mint(pow(other.n, self.n, self.MOD)) if isinstance(other, mint) else mint(pow(other, self.n, self.MOD))
+  def __add__(self, other_mint):
+    return mint(self.n + other_mint.n)
+  def __sub__(self, other_mint):
+    return mint(self.n - other_mint.n)
+  def __mul__(self, other_mint):
+    return mint(self.n * other_mint.n)
+  def __truediv__(self, other_mint):
+    return mint(self.n * mint.inv_extgcd(other_mint.n))
+  def __pow__(self, other_number):
+    return mint(pow(self.n, other_number, mint.MOD))
 
   def __pos__(self): return self
-  def __neg__(self): return 0-self
+  def __neg__(self): return mint(0 - self.n)
 
   def __eq__(self, other):
-    return self.n == other.n if isinstance(other, mint) else self.n == other%self.MOD
+    return self.n == other.n
   def __ne__(self, other):
     return not self.__eq__(other)
 
   def inv_extgcd(a):
-    u = 1; v = 0; b = MOD
+    u = 1; v = 0; b = mint.MOD
     while b:
       t = a // b
       a -= t * b; a, b=b, a
       u -= t * v; u, v=v, u
-    return u % MOD
+    return u % mint.MOD
