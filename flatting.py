92c096af4f6bda01a816a8d78e8688554eab58cc
ButterFlv <122111975+ButterFlv@users.noreply.github.com>
2024-07-01 00:12:53 +0900

    Update flatting.py

--- flatting.py
+++ flatting.py
@@ -1,10 +1,10 @@
-def press(__init, *__bases):
-    __B = bases + [1]
+def flatting(__init, *__bases):
+    __B = list(__bases) + [1]
     for i in range(len(__B)-2, -1, -1):
         __B[i] *= __B[i+1]
     __list = [__init]*__B[0]
     def __f(*__args):
-        return sum([__args[i]*__B[i+1] for __i in range(len(__B)-1)])
+        return sum([__args[__i]*__B[__i+1] for __i in range(len(__B)-1)])
     def __g(__n):
-        return [__n%__B[i]//__B[i+1] for __i in range(len(__B)-1)]
+        return [__n%__B[__i]//__B[__i+1] for __i in range(len(__B)-1)]
     return __list, __f, __g
