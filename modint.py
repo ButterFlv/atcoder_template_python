ddc2fdbfafbdc7119a394442f1f1bcdbae898033
ButterFlv <122111975+ButterFlv@users.noreply.github.com>
2024-07-02 22:51:32 +0900

    Update modint.py

--- modint.py
+++ modint.py
@@ -7,23 +7,16 @@ class mint:
   __repr__ = __str__
   def __int__(self): return self.n
 
-  def __add__(self, other):
-    return mint(self.n+other.n)if isinstance(other,mint)else mint(self.n+other)
-  def __sub__(self, other):
-    return mint(self.n-other.n)if isinstance(other,mint)else mint(self.n-other)
-  def __mul__(self, other):
-    return mint(self.n*other.n)if isinstance(other,mint)else mint(self.n*other)
-  def __truediv__(self, other):
-    return mint(self.n*mint.iegcd(other.n))if isinstance(other,mint)else mint(self.n*mint.iegcd(other))
-  def __pow__(self, other):
+  def __add__(self, other_mint):
+    return mint(self.n + other_mint.n)
+  def __sub__(self, other_mint):
+    return mint(self.n - other_mint.n)
+  def __mul__(self, other_mint):
+    return mint(self.n * other_mint.n)
+  def __truediv__(self, other_mint):
+    return mint(self.n * mint.inv_extgcd(other_mint.n))
+  def __pow__(self, other_number):
     return mint(pow(self.n, other_number, mint.MOD))
-
-  __radd__ = __add__
-  def __rsub__(self, other):
-    return mint(other.n-self.n)if isinstance(other,mint)else mint(other-self.n)
-  __rmul__ = __mul__
-  def __rtruediv__(self, other):
-    return mint(other.n*mint.iegcd(self.n))if isinstance(other,mint)else mint(other*mint.iegcd(self.n))
 
   def __pos__(self): return self
   def __neg__(self): return mint(0 - self.n)
@@ -33,7 +26,7 @@ class mint:
   def __ne__(self, other):
     return not self.__eq__(other)
 
-  def iegcd(a):
+  def inv_extgcd(a):
     u = 1; v = 0; b = mint.MOD
     while b:
       t = a // b
